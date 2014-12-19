ff66553240cb775a8335e5902fd542418362c5ca
Felix Schütz <fschuetz04@gmail.com>
2014-12-19 13:42:03 +0000

    Add AObject.getKeys() and AObject#getKeys() in aObject.js, bug fix

--- aObject.js
+++ aObject.js
@@ -18,26 +18,22 @@ AObject.prototype.compareKeys = function(object) {
 	AObject.compareKeys(this.object, object);
 };
 
+AObject.prototype.getKeys = function(keys) {
+	AObject.get(this.object, keys);
+};
+
 AObject.each = function(object, iterator, next) {
 	var length = Object.keys(object).length,
 		completed = 0,
 		root,
 		execute = true;
-	if(typeof next == 'undefined') {
-		next = function() {};
-	}
-	if (!length) {
-		return next();
-	}
-	for(var key in object) {
-		iterator(key, object[key], onlyOnce(done));
-	}
+	if(typeof next == 'undefined') next = function() {};
+	if (!length) return next();
+	for(var key in object) iterator(key, object[key], onlyOnce(done));
 	function onlyOnce(fn) {
         var called = false;
         return function() {
-            if (called) {
-				throw new Error('callback was already called');
-			}
+            if (called) throw new Error('callback was already called');
             called = true;
             fn.apply(root, arguments);
         };
@@ -49,9 +45,7 @@ AObject.each = function(object, iterator, next) {
 			return;
 		}
 		completed++;
-		if(completed >= length) {
-			next();
-		}
+		if(completed >= length) next();
 	}
 };
 
@@ -64,42 +58,40 @@ AObject.eachSync = function(object, iterator) {
 AObject.update = function(currentObject, newObject) {
 	Object.keys(newObject).forEach(function(key) {
 		if(typeof newObject[key] == 'object') {
-			if(typeof currentObject[key] == 'undefined') {
-				currentObject[key] = {};
-			}
+			if(typeof currentObject[key] == 'undefined') currentObject[key] = {};
 			return AObject.update(currentObject[key], newObject[key]);
 		}
 		currentObject[key] = newObject[key];
 	});
 };
 
-AObject.compareKeys = function(expectedObject, object, strict) {
+AObject.compareKeys = function(object, expectedObject, strict) {
 	var equal = true;
 	if(Array.isArray(expectedObject)) {
 		expectedObject.forEach(function(key) {
-			if(typeof object[key] == 'undefined') {
-				equal = false;
-			}
+			if(typeof object[key] == 'undefined') equal = false;
 		});
 	} else {
 		Object.keys(expectedObject).forEach(function(key) {
 			if(typeof expectedObject[key] == 'object') {
 				if(typeof object[key] == 'object') {
-					if(!AObject.compareKeys(expectedObject[key], object[key])) {
-						equal = false;
-					}
-				} else {
-					equal = false;
-				}
-			} else if(typeof object[key] == 'undefined') {
-				equal = false;
-			}
+					if(!AObject.compareKeys(expectedObject[key], object[key])) equal = false;
+				} else equal = false;
+			} else if(typeof object[key] == 'undefined') equal = false;
 		});
 	}
 	if(strict) {
-		AObject.compareKeys(object[key], expectedObject[key], true);
+		if(!AObject.compareKeys(expectedObject, object)) equal = false;
 	}
 	return equal;
+};
+
+AObject.getKeys = function(object, keys) {
+	returnObject = {};
+	keys.forEach(function(value) {
+		returnObject[value] = object[value];
+	});
+	return returnObject;
 };
 
 module.exports = AObject;
