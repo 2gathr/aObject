392223d23f2566a079a5b7b3ab6b6c439bd25f47
Robert Schütz <rschuetz17@gmail.com>
2015-02-26 10:00:48 +0100

    Use mocha for testing

---
+++ test/update.js
@@ -0,0 +1,93 @@
+(function() {
+  'use strict';
+
+  var assert = require('assert');
+  var objectAnalyzr = require('../objectAnalyzr');
+
+  describe('objectAnalyzr', function() {
+    describe('.update()', function() {
+
+      it('should replace objects on level 1 when depth=0', function() {
+        var object = {
+          one: 'foo',
+          nested: {
+            two: 'bar',
+            three: 'qux'
+          }
+        };
+        var extendingObject = {
+          nested: {
+            three: 'corge',
+            four: 'waldo'
+          },
+          five: 'fred'
+        };
+        objectAnalyzr.update(object, extendingObject, { depth: 0 });
+        assert.deepEqual(object, {
+          one: 'foo',
+          nested: {
+            three: 'corge',
+            four: 'waldo'
+          },
+          five: 'fred'
+        });
+      });
+
+      it('should update objects on level 1 when depth=1', function() {
+        var object = {
+          one: 'foo',
+          nested: {
+            two: 'bar',
+            three: 'qux'
+          }
+        };
+        var extendingObject = {
+          nested: {
+            three: 'corge',
+            four: 'waldo'
+          },
+          five: 'fred'
+        };
+        objectAnalyzr.update(object, extendingObject, { depth: 1 });
+        assert.deepEqual(object, {
+          one: 'foo',
+          nested: {
+            two: 'bar',
+            three: 'corge',
+            four: 'waldo'
+          },
+          five: 'fred'
+        });
+      });
+
+      it('should upate objects on any level when no depth option is given', function() {
+        var object = {
+          one: 'foo',
+          nested: {
+            two: 'bar',
+            three: 'qux'
+          }
+        };
+        var extendingObject = {
+          nested: {
+            three: 'corge',
+            four: 'waldo'
+          },
+          five: 'fred'
+        };
+        objectAnalyzr.update(object, extendingObject);
+        assert.deepEqual(object, {
+          one: 'foo',
+          nested: {
+            two: 'bar',
+            three: 'corge',
+            four: 'waldo'
+          },
+          five: 'fred'
+        });
+      });
+
+    });
+  });
+
+}());
